28c7687e849c203dc8053fef807526ebecbccf88
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2019-09-01 08:44:36 +0200

    Player Ship

---
+++ RailSpaceShooter/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        StartCoroutine(LoadFirstLevel());
+    }
+
+    private IEnumerator LoadFirstLevel()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
